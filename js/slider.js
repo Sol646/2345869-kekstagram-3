4a1560bf4ca127fa3eb7070d3b3fbd022fae1585
Sol646 <solodovnik.in@edu.spbstu.ru>
2023-05-24 21:33:52 +0300

    slider improved

--- js/slider.js
+++ js/slider.js
@@ -6,91 +6,89 @@ const effectValue = effectLevel.querySelector('.effect-level__value');
 let currentEffect = 'none';
 
 noUiSlider.create(slider, {
-    range: {
-      min: 0,
-      max: 100,
-    },
-    start: 100,
-    connect: 'lower'
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 100,
+  connect: 'lower'
 });
 
 const effects = {
-    chrome: {
-      settings: {
-        range: {
-          min: 0,
-          max: 1,
-        },
-        step: 0.1,
+  chrome: {
+    settings: {
+      range: {
+        min: 0,
+        max: 1,
       },
-      pattern: 'grayscale(',
-      units: '',
+      step: 0.1,
     },
-    sepia: {
-      settings: {
-        range: {
-          min: 0,
-          max: 1,
-        },
-        step: 0.1,
+    pattern: 'grayscale',
+    units: '',
+  },
+  sepia: {
+    settings: {
+      range: {
+        min: 0,
+        max: 1,
       },
-      pattern: 'sepia(',
-      units: '',
+      step: 0.1,
     },
-    marvin: {
-      settings: {
-        range: {
-          min: 0,
-          max: 100,
-        },
-        step: 1,
+    pattern: 'sepia',
+    units: '',
+  },
+  marvin: {
+    settings: {
+      range: {
+        min: 0,
+        max: 100,
       },
-      pattern: 'invert(',
-      units: '%',
+      step: 1,
     },
-    phobos: {
-      settings: {
-        range: {
-          min: 0,
-          max: 3,
-        },
-        step: 0.1,
+    pattern: 'invert',
+    units: '%',
+  },
+  phobos: {
+    settings: {
+      range: {
+        min: 0,
+        max: 3,
       },
-      pattern: 'blur(',
-      units: 'px',
+      step: 0.1,
     },
-    heat: {
-      settings: {
-        range: {
-          min: 1,
-          max: 3,
-        },
-        step: 0.1,
+    pattern: 'blur',
+    units: 'px',
+  },
+  heat: {
+    settings: {
+      range: {
+        min: 1,
+        max: 3,
       },
-      pattern: 'brightness(',
-      units: '',
+      step: 0.1,
     },
-  };
+    pattern: 'brightness',
+    units: '',
+  },
+};
 
-  slider.noUiSlider.on('update', () => {
-    effectValue.value = slider.noUiSlider.get();
-    if (currentEffect !== 'none') {
-        imagePreview.style.filter =
-         effects[currentEffect].pattern + slider.noUiSlider.get() + effects[currentEffect].units + ')';
-    }
-  });
+slider.noUiSlider.on('update', () => {
+  effectValue.value = slider.noUiSlider.get();
+  if (currentEffect !== 'none') {
+    imagePreview.style.filter = `${effects[currentEffect].pattern}(${slider.noUiSlider.get()}${effects[currentEffect].units})`;
+  }
+});
 
-  const changeEffect = (effect) => {
-    currentEffect = effect;
-    if (currentEffect === 'none') {
-        effectLevel.classList.add('hidden');
-        imagePreview.style.filter = '';
-      return;
-    }
-    effectLevel.classList.remove('hidden');
-    slider.noUiSlider.updateOptions(effects[currentEffect].settings);
-    slider.noUiSlider.set(effects[currentEffect].settings.range.max);
-  };
+const changeEffect = (effect) => {
+  currentEffect = effect;
+  if (currentEffect === 'none') {
+    effectLevel.classList.add('hidden');
+    imagePreview.style.filter = '';
+    return;
+  }
+  effectLevel.classList.remove('hidden');
+  slider.noUiSlider.updateOptions(effects[currentEffect].settings);
+  slider.noUiSlider.set(effects[currentEffect].settings.range.max);
+};
 
-  export { changeEffect };
-  
+export { changeEffect };
